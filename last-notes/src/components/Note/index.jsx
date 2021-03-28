d382c2af1bc3724785eedb06485303dc36c3d3c5
Tobias Heitor Goettert <tobias.goettert@compasso.com.br>
2021-03-28 10:43:31 -0300

    Creation of component NotesArea, Notes and Note.

---
+++ last-notes/src/components/Note/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./styles.css";
+
+export default function Note() {
+    return(
+        <div className="note">
+            <h2 className="title">Titulo exemplo</h2>
+            <hr/>
+            <p className="note-description">Descrição de exemplo</p>
+        </div>
+    )
+}
---
+++ last-notes/src/components/Notes/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Note from "../Note";
+
+import "./styles.css";
+
+export default function Notes() {
+    return (
+        <section className="notes">
+            <Note />
+        </section>
+    )
+}
