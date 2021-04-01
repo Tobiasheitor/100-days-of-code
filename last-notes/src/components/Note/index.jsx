10f7ab597f771324b95af4d8ab928399584a711f
Tobias Heitor Goettert <tobias.goettert@compasso.com.br>
2021-03-31 22:52:25 -0300

    #Day5 Creation of useState flag and custom highlight on click.

--- last-notes/src/components/Note/index.jsx
+++ last-notes/src/components/Note/index.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "./styles.css";
 
 export default function Note() {
+    const [hightlight, setHighlight] = useState(false);
+
     return(
-        <div className="note">
+        <div className={`note ${hightlight ? "hightlight" : ""}`}
+            onClick={() => {
+                setHighlight(!hightlight);
+            }}
+        >
             <h2 className="title">Titulo exemplo zap zap zap zap zap</h2>
             <hr/>
             <p className="note-description">
