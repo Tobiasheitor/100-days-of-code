2df87edc8a14613a900f51f6085c96d6c2e40068
Tobias Heitor Goettert <tobias.goettert@compasso.com.br>
2021-04-02 16:04:32 -0300

    #Day7 Iteration over noteList.

--- last-notes/src/components/Notes/index.jsx
+++ last-notes/src/components/Notes/index.jsx
@@ -1,19 +1,26 @@
 import React from "react";
+import { useHightLight } from "../../context/HightLightContext";
 import Note from "../Note";
+
 
 import "./styles.css";
 
 export default function Notes() {
+    const {noteList, setNoteList} = useNoteList();
+    const {hightlight, setHightLight} = useHightlight();
+
     return (
         <section className="notes">
-            <Note />
-            <Note />
-            <Note />
-            <Note />
-            <Note />
-            <Note />
-            <Note />
-            <Note />
+            {noteList.map((note) => (
+                <Note 
+                    key={note.id}
+                    id={note.id}
+                    title={note.title}
+                    description={note.description}
+                    hightlight={hightlight}
+                    setHighLight={setHightLight}
+                />
+            ))}
         </section>
     )
 }
