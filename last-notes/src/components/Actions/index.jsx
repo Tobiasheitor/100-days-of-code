cde55f851feb7d682dfa4848b11bc82842aba310
Tobias Heitor Goettert <tobias.goettert@compasso.com.br>
2021-04-12 23:03:30 -0300

    #Day10 Creation of actions edit and delete into Actions component.

--- last-notes/src/components/Actions/index.jsx
+++ last-notes/src/components/Actions/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { FaPlus, FaPencilAlt, FaTrash } from 'react-icons/fa';
 import { useNoteForm } from '../../context/NoteFormContext';
+import { useHightLight } from '../../context/HightLightContext';
+import { useNoteList } from '../../context/NoteListContext';
 
 import "./styles.css"
 
 export default function Actions() {
-    const { visibleForm, setVisibleForm } = useNoteForm();
+    const { visibleForm, setVisibleForm, setTitle, setDescription } = useNoteForm();
+    const { hightlight, setHightLight } = useHightLight();
+    const { noteList, setNoteList } = useNoteList();
 
     function createHandler() {
         if (visibleForm) {
@@ -14,16 +18,40 @@ export default function Actions() {
             setVisibleForm(true);
         }
     }
+
+    function editHandler() {
+        if (hightlight) {
+            const hightLightedNote = noteList.find((note)=> note.id === hightlight);
+
+            setTitle(hightLightedNote.title);
+            setDescription(hightLightedNote.description);
+            setVisibleForm(!visibleForm);
+        }
+    }
+
+    function deleteHandler() {
+        if (hightlight) {
+            setTitle("");
+            setDescription("");
+            setHightLight(false);
+
+            const hightLightedNoteIndex = noteList.findIndex((note) => note.id === hightlight);
+            noteList.splice(hightLightedNoteIndex, 1);
+
+            setNoteList([...noteList]);
+        }
+    }
+
     return (
         <div className="actions">
             <button className="create" onClick={createHandler}>
                 <FaPlus className="icon"></FaPlus>
             </button>
-            <button className="edit">
-                <FaPencilAlt className="icon disabled"></FaPencilAlt>
+            <button className="edit" onClick={editHandler}>
+                <FaPencilAlt className={`icon ${!hightlight ? "disabled" : ""}`} />
             </button>
-            <button className="delete">
-                <FaTrash className="icon disabled"></FaTrash>
+            <button className="delete" onClick={deleteHandler}>
+                <FaTrash className={`icon ${!hightlight && "disabled"}`} />
             </button>
         </div>
     )
