7a7d50103ae9b3e3a2d41d8bc13e05a1556ed248
Tobias Heitor Goettert <tobias.goettert@compasso.com.br>
2021-04-17 17:57:57 -0300

    #Day11 Addition of useEffect to verify selected note.

--- last-notes/src/components/Notes/index.jsx
+++ last-notes/src/components/Notes/index.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useHightLight } from "../../context/HightLightContext";
+import { useNoteForm } from "../../context/NoteFormContext";
 import { useNoteList } from "../../context/NoteListContext";
 import Note from "../Note";
 
@@ -8,7 +9,18 @@ import "./styles.css";
 export default function Notes() {
     const {noteList} = useNoteList();
     const {hightlight, setHightLight} = useHightLight();
+    const {setTitle, setDescription} = useNoteForm();
 
+    useEffect(() => {
+        if (hightlight) {
+            const highlightedNote = noteList.find((note) => note.id === hightlight);
+            setTitle(highlightedNote.title);
+            setDescription(highlightedNote.description);
+        } else {
+            setTitle("");
+            setDescription("");
+        }
+    }, [hightlight])
     return (
         <section className="notes">
             {noteList.map((note) => (
